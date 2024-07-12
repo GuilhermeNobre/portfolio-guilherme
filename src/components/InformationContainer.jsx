d2d421f2341df82f52f2e4687969cd18a79f62ea
Guilherme Nobre <guinobre1602@gmail.com>
2024-07-11 22:49:38 -0300

    feat: Update InformationContainer to use correct icon for address

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -5,7 +5,7 @@ import '../styles/components/informationcontainer.sass'
 const dataInfos = [
     {name:"phone", icon: <AiFillPhone id="phone-icon"/>, title: "Telefone", info:"(34) 99867-2460"},
     {name:"email", icon: <AiTwotoneMail id="mail-icon"/>, title: "E-Mail", info:"guinobre1602@gmail.com"},
-    {name:"phone", icon: <AiFillEnvironment id="enviroment-icon"/>, title: "Endereço", info:"Uberlândia-MG"},
+    {name:"enviroment", icon: <AiFillEnvironment id="enviroment-icon"/>, title: "Endereço", info:"Uberlândia-MG"},
 ]
 
 const InformationContainer = () => {
